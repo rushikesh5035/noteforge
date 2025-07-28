b5230f30d7444a41589792ed0b015a157fe4a7c2
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2025-07-28 16:03:03 +0530

    added github button and header logo

--- components/header.tsx
+++ components/header.tsx
@@ -59,7 +59,25 @@ export const HeroHeader = () => {
 
             <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
               <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
-                <ModeSwitcher />
+                <div className="flex items-center">
+                  <Link
+                    href="https://github.com/rushikesh5035/noteforge.git"
+                    target="_blank"
+                  >
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      className="flex items-center gap-2"
+                    >
+                      <svg viewBox="0 0 24 24" className="size-4 fill-current">
+                        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                      </svg>
+                    </Button>
+                  </Link>
+
+                  <ModeSwitcher />
+                </div>
+
                 <Button asChild variant="outline" size="sm">
                   <Link href="login">
                     <span>Login</span>
